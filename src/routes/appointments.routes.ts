cd1d47d053a13fd68dc54e1b659309daad1ef86e
André Treib <andre.schumacher.treib@gmail.com>
2020-07-02 17:34:36 -0300

    eslint problem

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,41 @@
 import { Router, Request, Response } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments: object[] = [];
+interface AppointmentModel {
+	id: string;
+	provider: string;
+	date: Date;
+}
 
-appointmentsRouter.get('/', (req, res) => {
+const appointments: AppointmentModel[] = [];
+
+appointmentsRouter.get('/', (req: Request, res: Response) => {
 	return res.json(appointments);
 });
 
-appointmentsRouter.post('/', (req, res) => {
+const findAppointmentByDate = (date: Date): AppointmentModel[] => {
+	return appointments.filter(r => isEqual(r.date, date));
+};
+
+appointmentsRouter.post('/', (req: Request, res: Response) => {
 	const { provider, date } = req.body;
-	const newAppointment = {
+	const parsedDate = startOfHour(parseISO(date));
+
+	const result = findAppointmentByDate(parsedDate);
+
+	if (result && result.length > 0)
+		return res.status(400).json({
+			message:
+				'Horário não disponível sabe como fica eu nao sei o que eu to fazendo',
+		});
+
+	const newAppointment: AppointmentModel = {
 		id: uuid(),
 		provider,
-		date,
+		date: parsedDate,
 	};
 	appointments.push(newAppointment);
 	return res.json(newAppointment);
