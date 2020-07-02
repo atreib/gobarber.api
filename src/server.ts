d82d3117755dcb2bafeee5d8b667cd7bd2f7d0f1
André Treib <andre.schumacher.treib@gmail.com>
2020-07-02 15:12:36 -0300

    configure format options

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.get('/', (request, response) => {
+	response.json({
+		message: 'Rota inicial',
+	});
+});
+
+app.listen(3333, () => {
+	console.log('*** server started on port 3333 ');
+});
