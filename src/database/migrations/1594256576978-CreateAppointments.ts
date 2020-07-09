4463e53920f558651b3e82ee773a5dc7e8b312ff
André Treib <andre.schumacher.treib@gmail.com>
2020-07-08 22:36:55 -0300

    add typeorm postgres and migrations

---
+++ src/database/migrations/1594256576978-CreateAppointments.ts
@@ -0,0 +1,36 @@
+/* eslint-disable class-methods-use-this */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+// eslint-disable-next-line prettier/prettier
+export default class CreateAppointments1594256576978 implements MigrationInterface {
+
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: 'appointments',
+				columns: [
+					{
+						name: 'id',
+						type: 'varchar',
+						isPrimary: true,
+						generationStrategy: 'uuid',
+					},
+					{
+						name: 'provider',
+						type: 'varchar',
+						isNullable: false,
+					},
+					{
+						name: 'date',
+						type: 'timestamp with time zone',
+						isNullable: false,
+					},
+				],
+			}),
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable('appointments');
+	}
+}
